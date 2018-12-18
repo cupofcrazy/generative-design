e7b7bcfd947bcf0da421189de830d2892ffe52bf
Tobi Balogun <tobybarlhogun@gmail.com>
2018-12-18 13:02:51 -0800

    update colorSpectrum.js

--- public/colorSpectrum.js
+++ public/colorSpectrum.js
@@ -1,23 +1,35 @@
-import * as p5 from 'p5'
-
 const config = {
-    width: 400,
-    height: 400
+    canvasWidth: 800,
+    canvasHeight: 400
 }
 
-const sketch = (p5) => {
+const colorSpectrumSketch = (p5) => {
+    const { canvasWidth, canvasHeight } = config
     // Grid
-    const Grid = (width) => {
+    const Grid = () => {
         let stepX, stepY;
-        createGrid()
         const createGrid = () => {
-            for (let gridY = 0; gridY < height; gridY += stepY) {
-                for (let gridX = 0; gridX < width; gridX += stepX) {
-                    p5.fill(gridX, height - gridY, 100)
+            stepX = p5.mouseX + 2
+            stepY = p5.mouseY + 2
+            for (let gridY = 0; gridY < p5.height; gridY += stepY) {
+                for (let gridX = 0; gridX < p5.width; gridX += stepX) {
+                    p5.fill(gridX, p5.height - gridY, 100)
                     p5.rect(gridX, gridY, stepX, stepY)
                     
-                } 
+                }
             }
         }
+        createGrid()   
+    }
+    p5.setup = () => {
+        p5.createCanvas(canvasWidth, canvasHeight)
+        p5.noStroke()
+        p5.colorMode(p5.HSB, p5.width, p5.height, 100)
+        // p5.noStroke()
+    }
+    p5.draw = () => {
+        Grid()
     }
 }
+
+export  { colorSpectrumSketch }
