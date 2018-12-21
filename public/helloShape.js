7ed368871aaee92fd0d4302adcf60d9ea7407af7
Tobi Balogun <tobybarlhogun@gmail.com>
2018-12-21 00:06:46 -0800

    update

---
+++ public/helloShape.js
@@ -0,0 +1,23 @@
+const config = { width: 500, height: 500 }
+
+const shapeSketch = (p5) => {
+    // const { top, left } 
+    const { width, height } = config
+    const centerX = width / 2
+    const centerY = height / 2
+
+    p5.setup = () => {
+        p5.createCanvas( width, height )
+       
+    }
+    p5.draw = () => {
+        p5.background(255)
+        p5.translate(centerX, centerY)
+
+        const circleRes = p5.map(p5.mouseY, 0, height, 2, 80)
+        
+        
+    }
+}
+
+export { shapeSketch }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import p5 from 'p5';
 import { sketch } from '../public/sketch'
 import { colorSpectrumSketch } from '../public/colorSpectrum'
+import { shapeSketch } from '../public/helloShape'
 
-const P5 = new p5(colorSpectrumSketch)
+const P5 = new p5(shapeSketch)
